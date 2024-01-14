801138eda0c286dfc607cb2c02dadfa09e5652c3
Nahuel Uliassi Pirchio <uliassipirchio@gmail.com>
2024-01-14 18:05:51 -0300

    [🔱 Update] Fav Status

--- main.js
+++ main.js
@@ -1,32 +1,65 @@
 const { BrowserWindow, app, ipcMain } = require('electron')
+const fs = require('fs')
 const { exec } = require('child_process')
 const path = require('path')
 
 const ips = ['192.168.0.3', '192.168.0.9']
 
-async function handleChangeColor (event, ip, { r, g, b }, dimming) {
+async function handleChangeColor (_event, ip, { r, g, b }, dimming) {
   return execFunction(`echo -n '{"id":1,"method":"setPilot","params":{"r":${r},"g":${g},"b":${b},"dimming": ${dimming}}}' | nc -u -w 1 ${ip} 38899`)
 }
 
-async function handleSetTemp (event, ip, temp, dimming) {
+async function handleSetTemp (_event, ip, temp, dimming) {
   return execFunction(`echo -n '{"id":1,"method":"setPilot","params":{"temp":${temp},"dimming": ${dimming}}}' | nc -u -w 1 ${ip} 38899`)
 }
 
-async function handleSetScene (event, ip, sceneId, sceneSpeed, dimming) {
+async function handleSetScene (_event, ip, sceneId, sceneSpeed, dimming) {
   return execFunction(`echo -n '{"id":1,"method":"setPilot","params":{"sceneId":${sceneId},"speed": ${sceneSpeed},"dimming": ${dimming}}}' | nc -u -w 1 ${ip} 38899`)
 }
 
-async function handleSetBulb (event, ip, state) {
+async function handleSetBulb (_event, ip, state) {
   return execFunction(`echo -n '{"id":1,"method":"setState","params":{"state":${state}}}' | nc -u -w 1 ${ip} 38899`)
 }
 
-async function handleGetBulbs (event) {
+async function handleGetBulbs (_event) {
   const bulbsRequests = ips.map(ip => execFunction(`echo -n '{"method":"getPilot","params":{}}' | nc -u -w 1 ${ip} 38899`).catch(e => console.error(e)))
   const responses = await Promise.all(bulbsRequests)
   return responses.map((response, index) => {
     if (response instanceof Error || !response) return ''
     return { ...response, ip: ips[index] }
   })
+}
+
+async function handleAddData (_event, data, filePath) {
+  const fileExists = fs.existsSync(filePath)
+  let existingData = []
+
+  if (fileExists) {
+    const fileContent = fs.readFileSync(filePath, {
+      encoding: 'utf-8'
+    })
+    existingData = JSON.parse(fileContent)
+  }
+
+  existingData.push(data)
+
+  const updatedJsonData = JSON.stringify(existingData)
+
+  fs.writeFileSync(filePath, updatedJsonData, {
+    encoding: 'utf-8'
+  })
+
+  console.log('Data added to', filePath)
+}
+// async function handleSaveData (_event, data, path) {
+//   const jsonData = JSON.stringify(data)
+//   console.log(path)
+//   fs.writeFileSync(path, jsonData)
+//   console.log('data saved')
+// }
+async function handleGetData (_event, path) {
+  const data = fs.readFileSync(path)
+  return JSON.parse(data)
 }
 
 const createWindow = () => {
@@ -43,11 +76,17 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+  const userDataFilePath = app.getPath('userData')
+
   ipcMain.handle('setBulb', handleSetBulb)
   ipcMain.handle('getBulbs', handleGetBulbs)
   ipcMain.handle('changeColor', handleChangeColor)
   ipcMain.handle('setTemp', handleSetTemp)
   ipcMain.handle('setScene', handleSetScene)
+
+  ipcMain.handle('addStatus', (event, ...args) => handleAddData(event, args, path.join(userDataFilePath, 'status.json')))
+  ipcMain.handle('getStatus', (event, ..._args) => handleGetData(event, path.join(userDataFilePath, 'status.json')))
+
   createWindow()
 
   app.on('activate', () => {
@@ -65,7 +104,7 @@ app.on('window-all-closed', () => {
 
 function execFunction (functionStatement) {
   return new Promise((resolve, reject) => {
-    exec(functionStatement, (error, stdout, stderr) => {
+    exec(functionStatement, (error, stdout, _stderr) => {
       if (error) {
         reject(error)
       } else {
